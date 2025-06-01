b29dbc4f9e11b3da9a0deee15b84b67d5043089d
shubham kamboj <shubham.s.kamboj@gmail.com>
2025-06-01 11:01:44 +0530

    changes in controller

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, World! static Data";
+        return "Hello, World! static Data- check for services";
     }
 }
